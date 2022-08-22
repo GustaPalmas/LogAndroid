46c0f752f6cffa9d1327a5e9db8c2ff6afe93bf8
Gustapalmas <gustavo.p1995@aluno.ifsc.edu.br>
2022-08-22 17:12:47 -0300

    Aula03-22/08, alterações

--- app/src/main/java/com/br/edu/ifsc/palmas/logveiculo/ui/motorista/CadMotoristaFragment.java
+++ app/src/main/java/com/br/edu/ifsc/palmas/logveiculo/ui/motorista/CadMotoristaFragment.java
@@ -1,4 +1,4 @@
-package com.br.edu.ifsc.palmas.logveiculo;
+package com.br.edu.ifsc.palmas.logveiculo.ui.motorista;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.br.edu.ifsc.palmas.logveiculo.R;
 
 /**
  * A simple {@link Fragment} subclass.
---
+++ app/src/main/java/com/br/edu/ifsc/palmas/logveiculo/ui/motorista/Motorista.java
@@ -0,0 +1,122 @@
+package com.br.edu.ifsc.palmas.logveiculo.ui.motorista;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Motorista {
+    //atributos
+    private boolean aceito;
+    private String nome;
+    private String email;
+    private String senha;
+    private String data;
+    private byte categoria;
+    private long cpf;
+    private long cnh;
+
+    //metodos
+    public boolean isAceito() {
+        return aceito;
+    }
+
+    public void setAceito(boolean aceito){
+        this.aceito=aceito;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setData(String data) {
+        SimpleDateFormat myFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date date = (Date) myFormat.parse(data);
+            this.data = data;
+        } catch (ParseException e) {
+            this.data="";
+            }
+        }
+
+    public long getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(long cnh){
+        this.cnh=cnh;
+    }
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf){
+        this.cpf=cpf;
+    }
+
+    public byte getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(byte categoria){
+        this.categoria=categoria;
+    }
+
+    //construtores
+    public Motorista(JSONObject jp){
+        try{
+            Integer conversor = (int) jp.get("cpf");
+            this.setCpf(conversor);
+            this.setNome(String) jp.get("nome");
+            this.setSenha(String); jp.get("senha");
+            this.setEmail(String); jp.get("email");
+            boolean bool = Boolean.getBoolean(jp.get("aceito").toString());
+            this.setAceito(bool);
+            this.setData(String); jp.get("data");
+            Byte conversor = (long) jp.get("cnh");
+            this.setCnh(conversor);
+            Byte conversor = (byte) jp.get("categoria");
+            this.setCategoria(conversor);
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+    //CONSTRUTOR - Inicializa os atributos para gerar Objeto Json
+    public Motorista() {
+        this.setCategoria(0);
+        this.setAceito(false);
+        this.setCpf(0);
+        this.setCnh(0);
+        this.setData(0);
+        this.setNome("");
+        this.setSenha("");
+        this.setEmail("");
+    }
+    //Metodo retorna o objeto com dados no formato JSON
+    public JSONObject toJsonObject () {
+        JSONObject json = new JSONObject();
+
+    }
+}
