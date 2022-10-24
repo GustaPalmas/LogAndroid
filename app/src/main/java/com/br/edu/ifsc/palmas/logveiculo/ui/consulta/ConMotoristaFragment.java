eece4cc612229d545c723c142330d6228f09eb63
GustaPalmas <gustavo.p1995@aluno.ifsc.edu.br>
2022-10-24 17:39:15 -0200

    24-10-22-16:39

--- app/src/main/java/com/br/edu/ifsc/palmas/logveiculo/ui/consulta/ConMotoristaFragment.java
+++ app/src/main/java/com/br/edu/ifsc/palmas/logveiculo/ui/consulta/ConMotoristaFragment.java
@@ -13,12 +13,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.br.edu.ifsc.palmas.logveiculo.R;
 import com.br.edu.ifsc.palmas.logveiculo.ui.motorista.Motorista;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -69,29 +77,102 @@ public class ConMotoristaFragment extends Fragment implements
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_con_motorista_list, container, false);
+        this.view = inflater.inflate(R.layout.fragment_con_motorista_list, container, false);
+        //instanciando a fila de requests - caso o objeto seja o view
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
+        //inicializando a fila de requests do SO
+        this.requestQueue.start();
+        //array parametro de envio para o serviço
+        JSONArray jsonArray = new JSONArray();
+        //objeto com informações de filtro da consulta
+        Motorista motorista = new Motorista();
+        motorista.setId("");
+        motorista.setNome("");
+        motorista.setEmail("");
+        motorista.setCpf("");
+        //incluindo objeto no array de envio
+        jsonArray.put(motorista.toJsonObject());
+//requisição para o Rest Server
+        jsonArrayReq = new JsonArrayRequest(Request.Method.GET,
+                "http://10.0.2.2:8080/motoristarest/rest/motorista",
+                jsonArray, this, this);
+//mando executar a requisção na fila do sistema
+        requestQueue.add(jsonArrayReq);
 
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new ConMotoristaRecyclerViewAdapter(???));
-        }
-        return view;
+        return this.view;
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
+        //mostrar mensagem que veio do servidor
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar a consulta: " +
+                        error.toString(), Snackbar.LENGTH_LONG);
+        mensagem.show();
+
 
     }
 
     @Override
     public void onResponse(Object response) {
-
+        try {
+            //array Json para receber a resposta do webservice
+            JSONArray jsonArray = null;
+            jsonArray = new JSONArray(response.toString());
+            //se a consulta não veio vazia passar para array list
+            if (jsonArray != null) {
+                //objeto java
+                Motorista motorista = null;
+                //array list para receber a resposta
+                this.motorista = new ArrayList<Motorista>();
+                //preenchendo ArrayList com JSONArray recebido
+                for (int i = 0, size = jsonArray.length();
+                     i < size; i++) {
+                    JSONObject jo = jsonArray.getJSONObject(i);
+                    motorista = new Motorista(jo);
+                    this.motorista.add(motorista);
+                }
+                // Set the adapter
+                if (view instanceof RecyclerView) {
+                    Context context = view.getContext();
+                    RecyclerView recyclerView = (RecyclerView) view;
+                    if (mColumnCount <= 1) {
+                        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                    } else {
+                        recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+                    }
+                    recyclerView.setAdapter(new ConMotoristaRecyclerViewAdapter(this.motorista));
+                }
+                /*
+                O código abaixo já estava no método onCreateView().
+                Mas foi movido para cá, porque só pode ser
+                executado se o this.sensores for válido.
+                Ou seja, se NÃO retornar dados da consulta,
+                ele não deve ser executado;
+                */
+                if (view instanceof RecyclerView) {
+                    Context context = view.getContext();
+                    RecyclerView recyclerView =
+                            (RecyclerView) view;
+                    if (mColumnCount <= 1) {
+                        recyclerView.setLayoutManager(
+                                new LinearLayoutManager(context));
+                    } else {
+                        recyclerView.setLayoutManager(
+                                new GridLayoutManager(context,
+                                        mColumnCount));
+                    }
+                    recyclerView.setAdapter(
+                            new ConMotoristaRecyclerViewAdapter(this.motorista));
+                }
+            }else {
+                Snackbar mensagem = Snackbar.make(view,
+                        "A consulta não retornou nenhum registro!",
+                        Snackbar.LENGTH_LONG);
+                mensagem.show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
-}
+    }
